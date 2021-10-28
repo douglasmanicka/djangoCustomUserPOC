7b6471d5312b8d5e196e030c1f13487b97ad63d5
Douglas Manicka <douglasmanicka@gmail.com>
2021-10-28 20:07:17 -0300

    customizing account from AbstractUser

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
 
-# Register your models here.
+from .models import Account
+admin.site.register(Account, auth_admin.UserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class Account(AbstractUser):
+    ACCOUNT_TYPES = (
+        (u'T', u'Teacher'),
+        (u'A', u'administrative'),
+    )
+    type = models.CharField(max_length=14, choices=ACCOUNT_TYPES)
+    

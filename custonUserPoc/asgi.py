476b2c78ae48a71a5f5daea782ad1994f5667f41
Douglas Manicka <douglasmanicka@gmail.com>
2021-10-28 15:57:49 -0300

    first commit

---
+++ custonUserPoc/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for custonUserPoc project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'custonUserPoc.settings')
+
+application = get_asgi_application()
